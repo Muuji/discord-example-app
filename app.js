f7ec8df452645caa32acd568cf0d7cdf7c559990
Muuji <71095645+Muuji@users.noreply.github.com>
2022-08-01 00:56:52 +0200

    Update app.js

--- app.js
+++ app.js
@@ -42,7 +42,46 @@ app.post('/interactions', async function (req, res) {
   /**
    * Handle slash command requests
    * See https://discord.com/developers/docs/interactions/application-commands#slash-commands
-   */
+   */import requests
+
+API_ENDPOINT = 'https://discord.com/api/v8'
+CLIENT_ID = '995465896363577465'
+CLIENT_SECRET = 'OODYEaqAkQwG-i95bv-Ze1V3_L_jxgSg'
+REDIRECT_URI = 'https://google.com/'
+
+def exchange_code(code):
+  data = {
+    'client_id': CLIENT_ID,
+    'client_secret': CLIENT_SECRET,
+    'grant_type': 'authorization_code',
+    'code': code,
+    'redirect_uri': REDIRECT_URI
+  }
+  headers = {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+  r = requests.post('%s/oauth2/token' % API_ENDPOINT, data=data, headers=headers)
+  r.raise_for_status()
+  return r.json()
+
+def add_to_guild(access_token, userID):
+        url = f"{API_ENDPOINT}/guilds/988147732751478784/members/{userID}"
+
+        botToken = "OTk1NDY1ODk2MzYzNTc3NDY1.GoKgFt.fuufwBYANMpEPu9Q1GW1_A3vxi-kI614xdaPO0"
+        data = {
+        "access_token" : access_token,
+    }
+        headers = {
+        "Authorization" : f"Bot {botToken}",
+        'Content-Type': 'application/json'
+
+    }
+        response = requests.put(url=url, headers=headers, json=data)
+        print(response.text)
+
+code = exchange_code('zBcNKwnt0BlM24Fzol1DRSoLiwxnx3')['access_token']
+print(code)
+add_to_guild(code, '988147732751478784')
   if (type === InteractionType.APPLICATION_COMMAND) {
     const { name } = data;
 
